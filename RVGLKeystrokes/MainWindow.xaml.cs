9923ca9e2767c52de8beb724dd05e7b46e7c43c1
Plextora <71889427+Plextora@users.noreply.github.com>
2023-06-16 19:29:25 -0400

    Added MouseKeyHook

--- RVGLKeystrokes/MainWindow.xaml.cs
+++ RVGLKeystrokes/MainWindow.xaml.cs
@@ -3,20 +3,8 @@
  * Licensed under the GPL-3.0 license (https://www.gnu.org/licenses/gpl-3.0.en.html#license-text)
  */
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Gma.System.MouseKeyHook;
 
 namespace RVGLKeystrokes
 {
