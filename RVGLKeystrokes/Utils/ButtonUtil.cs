265604cfe7523470592e224d3d8d1ad32d1a58ee
Plextora <71889427+Plextora@users.noreply.github.com>
2023-06-18 12:01:07 -0400

    Added activate button functionality
    Next is configuration support?

--- RVGLKeystrokes/Utils/ButtonUtil.cs
+++ RVGLKeystrokes/Utils/ButtonUtil.cs
@@ -7,17 +7,25 @@ namespace RVGLKeystrokes.Utils
 {
     public class ButtonUtil
     {
-        public static IEnumerable<Button> FindAllButtons(DependencyObject depObj)
-        {
-            return null; // for now
-        }
+        private static Brush _backgroundBorderValue;
+        private static Brush _pressedValue;
+        private static Brush _foregroundValue;
+        private static string _buttonBorderValue;
+        private static string _pressedButtonValue;
+        private static string _buttonForeground;
 
-        public static void ActivateButton(Button button)
-        {
-        }
+        private const string DefaultPressedValue = "#FFBEE6FD";
+        private const string DefaultBackgroundBorderValue = "#FFDDDDDD";
+        private readonly string _defaultForegroundValue = "#FF000000";
+        private readonly Brush _emptyBrush = HexToBrush("#00FFFFFF");
 
-        public static void DeactivateButton(Button button)
-        {
-        }
+        public static IEnumerable<Button> FindAllButtons(DependencyObject depObj) => null; // for now
+
+        public static void ActivateButton(Button button) => button.Background = HexToBrush(DefaultPressedValue);
+
+        public static void DeactivateButton(Button button) =>
+            button.Background = HexToBrush(DefaultBackgroundBorderValue);
+
+        private static Brush HexToBrush(string hex) => new BrushConverter().ConvertFrom(hex) as Brush;
     }
 }
